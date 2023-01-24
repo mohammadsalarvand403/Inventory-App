26674a6b5cd167f369ee716f4b4fba33ca68027d
mmd <mmdslv7707@gmail.com>
2023-01-24 21:37:35 +0330

    add to Navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-    <div className='bg-slate-800 min-h-screen'>
-      <div className='h-12 flex items-center justify-center gap-x-4 bg-slate-700 mb-6'>
-       <h1 className='md:text-xl text-sm font-bold text-slate-300'>
-       Inventory App using tailwind &Js
-       </h1>
-       <span className='flex items-center justify-center w-7 h-7 rounded-full
-       bg-slate-500 border-2 border-slate-300 font-bold text-slate-300'>
-        7
-       </span>
-      </div>
-    </div>
+  <Navbar/>
     </div>
   );
 }
